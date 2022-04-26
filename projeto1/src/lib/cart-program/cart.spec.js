43dcd6b93a5ef8f2b9a7f37376cc8f79223fa0a6
Dan-Sillva <danilo.official22@hotmail.com>
2022-04-26 17:58:51 -0300

    aula-16

--- projeto1/src/lib/cart-program/cart.spec.js
+++ projeto1/src/lib/cart-program/cart.spec.js
@@ -2,23 +2,39 @@ import Cart from './cart';
 
 describe('Cart', () => {
    let cart;
+   let product = {
+      tittle: 'Adidas running shoes - men',
+      price: 35388, // 353.88 | R$ 353,88
+   };
+
    beforeEach(() => (cart = new Cart()));
 
    it('espera-se retornar zero de um getTotal() de uma nova instancia de Cart', () => {
       expect(cart.getTotal()).toEqual(0);
    });
 
-   it('espera-se receber o total do carrinho de compras', () => {
+   it('espera-se receber o total do carrinho de compras [add]', () => {
       const item = {
-         product: {
-            tittle: 'Adidas running shoes - men',
-            price: 35388, // 353.88 | R$ 353,88
-         },
+         product,
          quantity: 2, // 70776
       };
 
       cart.add(item);
 
       expect(cart.getTotal()).toEqual(70776);
+   });
+
+   it('espera-se que somente um produto exista por vez', () => {
+      cart.add({
+         product,
+         quantity: 2,
+      });
+
+      cart.add({
+         product,
+         quantity: 1,
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
    });
 });
